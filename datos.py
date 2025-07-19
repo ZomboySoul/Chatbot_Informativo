17771be3f77fecd682022de5e5b2217d4defaf52
ZomboySoul <tatilezcano03@gmail.com>
2025-07-19 06:50:47 -0300

    Add global variables for academic programs and contact information

---
+++ datos.py
@@ -0,0 +1,88 @@
+# LISTAS GLOBALES 
+CARRERAS_INFO = [
+    {
+        "nombre": "Profesorado de Educación Inicial",
+        "duracion": "4 años",
+        "descripcion": "Formación para educadores de nivel inicial.",
+        "requisitos": "Título de Escuela Secundaria",
+        "plan_estudio": [
+            "1er año: Introducción a la educación, psicología infantil, didáctica general",
+            "2do año: Desarrollo cognitivo, juego y aprendizaje, prácticas docentes I",
+            "3er año: Diseño curricular, evaluación educativa, prácticas docentes II",
+            "4to año: Gestión institucional, proyectos educativos, residencia docente"
+        ],
+        "inscripcion": "Marzo-Abril / Agosto-Septiembre",
+        "modalidad": "Presencial"
+    },
+    {
+        "nombre": "Profesorado de Inglés",
+        "duracion": "4 años",
+        "descripcion": "Formación para docentes de inglés con enfoque comunicativo.",
+        "requisitos": "Título de Escuela Secundaria",
+        "plan_estudio": [
+            "1er año: Lengua Inglesa I, Fonética, Gramática Contrastiva",
+            "2do año: Literatura Angloamericana, Didáctica Especial, Observación de Clases",
+            "3er año: Lingüística Aplicada, Evaluación Educativa, Práctica Docente I",
+            "4to año: Seminario de Investigación, Práctica Docente II, Residencia"
+        ],
+        "inscripcion": "Marzo-Abril / Agosto-Septiembre",
+        "modalidad": "Presencial"
+    },
+    {
+        "nombre": "Tecnicatura Superior en Enfermería",
+        "duracion": "3 años",
+        "descripcion": "Formación técnica en enfermería general.",
+        "requisitos": "Título de Escuela Secundaria",
+        "plan_estudio": [
+            "1er año: Anatomía, Fisiología, Fundamentos de Enfermería",
+            "2do año: Farmacología, Enfermería Médico-Quirúrgica, Prácticas Hospitalarias",
+            "3er año: Salud Pública, Gestión en Enfermería, Práctica Profesional Supervisada"
+        ],
+        "inscripcion": "Febrero-Marzo / Julio-Agosto",
+        "modalidad": "Presencial"
+    },
+    {
+        "nombre": "Tecnicatura Superior en Ciencia de Datos e IA",
+        "duracion": "3 años",
+        "descripcion": "Formación en análisis de datos e inteligencia artificial aplicada.",
+        "requisitos": "Título de Escuela Secundaria",
+        "plan_estudio": [
+            "1er año: Programación I, Matemática Discreta, Estadística",
+            "2do año: Bases de Datos, Aprendizaje Automático, Visualización de Datos",
+            "3er año: Big Data, Ética en IA, Proyecto Integrador"
+        ],
+        "inscripcion": "Marzo a Mayo",
+        "modalidad": "Semipresencial"
+    },
+    {
+        "nombre": "Especialización en Enfermería en Salud Mental",
+        "duracion": "1 año",
+        "descripcion": "Formación avanzada para enfermeros en salud mental.",
+        "requisitos": "Título de Enfermería Profesional, Universitaria o Licenciatura",
+        "plan_estudio": [
+            "Módulo 1: Fundamentos de Salud Mental y Psiquiatría",
+            "Módulo 2: Intervenciones en Crisis y Rehabilitación",
+            "Módulo 3: Práctica Clínica Supervisada"
+        ],
+        "inscripcion": "Todo el año",
+        "modalidad": "Virtual"
+    }
+]
+CONTACTO = {
+    "direccion": "Franklin Nº 166 e/ Alvear y Libres del Sur, Chascomús",
+    "telefono": "(2241) 436710",
+    "email": "isfdyt57chascomus@abc.gob.ar",
+    "web": "https://isfdyt57chascomus.edu.ar/",
+    "redes": {
+        "facebook": "@Instituto57chascomus",
+        "instagram": "@Instituto57chascomus"
+    }
+}
+REQUISITOS = [
+    "DNI (original y fotocopia de anverso y reverso)",
+    "CUIL (original y fotocopia)",
+    "Partida de nacimiento (original y copia)",
+    "Certificado de aptitud física (vigente)",
+    "Título secundario (original y copia legalizada)",
+    "4 fotos 4x4 color (fondo blanco)"
+]
