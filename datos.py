e4c24bb3995e62ffea37399d96e491105422bd92
ZomboySoul <bytechsuppor@gmail.com>
2025-07-31 07:51:19 -0300

    updated career descriptions

--- datos.py
+++ datos.py
@@ -3,7 +3,7 @@ CARRERAS_INFO = [
     {
         "nombre": "Profesorado de Educación Inicial",
         "duracion": "4 años",
-        "descripcion": "Formación para educadores de nivel inicial.",
+        "descripcion": "Carrera docente orientada a la formación de profesionales capaces de acompañar el desarrollo integral de niños y niñas en el nivel inicial.",
         "requisitos": "Título de Escuela Secundaria",
         "plan_estudio": [
             "1er año: Introducción a la educación, psicología infantil, didáctica general",
@@ -17,7 +17,7 @@ CARRERAS_INFO = [
     {
         "nombre": "Profesorado de Inglés",
         "duracion": "4 años",
-        "descripcion": "Formación para docentes de inglés con enfoque comunicativo.",
+        "descripcion": "Formación integral para futuros docentes del idioma inglés, con un enfoque comunicativo, pedagógico y cultural.",
         "requisitos": "Título de Escuela Secundaria",
         "plan_estudio": [
             "1er año: Lengua Inglesa I, Fonética, Gramática Contrastiva",
@@ -31,7 +31,7 @@ CARRERAS_INFO = [
     {
         "nombre": "Tecnicatura Superior en Enfermería",
         "duracion": "3 años",
-        "descripcion": "Formación técnica en enfermería general.",
+        "descripcion": "Formación técnica orientada a la atención integral del paciente en diferentes niveles del sistema de salud.",
         "requisitos": "Título de Escuela Secundaria",
         "plan_estudio": [
             "1er año: Anatomía, Fisiología, Fundamentos de Enfermería",
@@ -44,7 +44,7 @@ CARRERAS_INFO = [
     {
         "nombre": "Tecnicatura Superior en Ciencia de Datos e IA",
         "duracion": "3 años",
-        "descripcion": "Formación en análisis de datos e inteligencia artificial aplicada.",
+        "descripcion": "Carrera técnica enfocada en el análisis, procesamiento y visualización de datos, así como en el diseño de soluciones inteligentes usando inteligencia artificial.",
         "requisitos": "Título de Escuela Secundaria",
         "plan_estudio": [
             "1er año: Programación I, Matemática Discreta, Estadística",
@@ -57,7 +57,7 @@ CARRERAS_INFO = [
     {
         "nombre": "Especialización en Enfermería en Salud Mental",
         "duracion": "1 año",
-        "descripcion": "Formación avanzada para enfermeros en salud mental.",
+        "descripcion": "Trayecto de formación avanzada dirigido a profesionales de enfermería que deseen intervenir en el campo de la salud mental con herramientas clínicas y éticas.",
         "requisitos": "Título de Enfermería Profesional, Universitaria o Licenciatura",
         "plan_estudio": [
             "Módulo 1: Fundamentos de Salud Mental y Psiquiatría",
@@ -68,6 +68,7 @@ CARRERAS_INFO = [
         "modalidad": "Virtual"
     }
 ]
+
 CONTACTO = {
     "direccion": "Franklin Nº 166 e/ Alvear y Libres del Sur, Chascomús",
     "telefono": "(2241) 436710",
