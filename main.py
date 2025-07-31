10b288cc11b089e1add0b3d6b7124529f86380cd
ZomboySoul <bytechsuppor@gmail.com>
2025-07-31 07:54:51 -0300

    Remove unused sections and update main logic

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ a cada sección del sistema informativo del Instituto Superior N°57.
 
 
 from interfaz import mostrar_bienvenida, mostrar_menu, despedida
-from secciones import carrera, modalidad, contacto, requisitos, redes, especializacion
+from secciones import carrera, contacto, requisitos
 from utils import limpiar_pantalla
 from colorama import Fore, Style
 
@@ -18,26 +18,23 @@ def iniciar_chatbot():
     
     menu_opciones = {
        "1": carrera,
-       "2": modalidad,
-       "3": contacto,
-       "4": requisitos,
-       "5": redes,
-       "6": especializacion,
-       "7": lambda: despedida(nombre_completo)
+       "2": contacto,
+       "3": requisitos,
+       "4": lambda: despedida(nombre_completo)
     }
 
     nombre_completo = mostrar_bienvenida()
     
     while True:
         limpiar_pantalla()
-        mostrar_menu()
-        opcion = input(Fore.LIGHTMAGENTA_EX + "\nIngrese una opción (1-7): " + Fore.RESET).strip()
+        
+        opcion = mostrar_menu()
 
         accion = menu_opciones.get(opcion)
 
         if accion:
             accion()
-            if opcion == "7":
+            if opcion == "4":
                 return  # sale del programa
         else:
             print(Fore.RED + "\n⚠️ Opción no válida. Por favor ingrese un número del 1 al 7." + Style.RESET_ALL)
